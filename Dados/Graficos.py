4639ef8a36df64a28fd2715b06fb679bb067ca4a
Marcelo Sander <marcelo.sander@hvarconsulting.com.br>
2025-06-03 22:24:27 -0300

    fix year in grafic and split

--- Dados/Graficos.py
+++ Dados/Graficos.py
@@ -15,20 +15,20 @@ Este painel compara os investimentos, crescimento da frota de ônibus elétricos
 with st.sidebar:
     selecionado = option_menu(
         menu_title='Menu',
-        options=['Frotas'],
+        options=['Frotas', 'Emissões de gases poluentes'],
         default_index=0
     )
 #frotas
 if selecionado == "Frotas":
     st.header("Crescimento da Frota Elétrica (Ônibus a Bateria – 2020–2025)")
     crescimento = {
-        "Ano": [2020, 2021, 2022, 2023, 2024, 2025],
-        "São Paulo": [0, 20, 50, 150, 299, 527],  
-        "Nova York": [10, 15, 25, 60, 75, 81]     
+        "Ano": ["2020", "2021", "2022", "2023", "2024", "2025"],
+        "São Paulo": [0, 20, 50, 150, 299, 527],
+        "Nova York": [10, 15, 15, 60, 81, 81]
     }
     df_crescimento = pd.DataFrame(crescimento).set_index("Ano")
     st.line_chart(df_crescimento)
-
+elif selecionado == "Emissões de gases poluentes":
     st.header("Comparação de Ônibus a Combustão")
     onibus = {
         "Cidade": ["Nova York", "São Paulo"],
@@ -44,4 +44,3 @@ if selecionado == "Frotas":
     }
     df_combustao = pd.DataFrame(combustao).set_index("Cidade")
     st.bar_chart(df_combustao)
-
